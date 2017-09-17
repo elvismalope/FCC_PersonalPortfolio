598605609a6f1f4146d5a3d24a82f89a5b8cb358
Elvis Malope <elvism@mip.co.za>
2017-09-17 17:32:14 +0200

    Change the Sceen to Ubuntu and Added the Intro text and Replaced the Images in portfolio

--- js/main.js
+++ js/main.js
@@ -5,3 +5,14 @@ $(function(){
 		navbarMenu.collapse('hide');
 	});
 });
+
+function openEmailClient() {
+	var message 			= $("#message_1").val();
+	var name 					= $("#name_1").val();
+	var contactNumber = $("#phone_1").val();
+	var subject = "Mail From emDev Website";
+	var body  = message + ".\n " + "Name: " + name + ".\n " + "Contact Number: " + contactNumber;
+
+	window.open("mailto:juzilopes@gmail.com?" + "subject=" + subject + "&body=" + body);
+
+}
